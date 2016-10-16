9a45c88536eb6da177c18fde35f98cd1be0646e6
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-16 12:27:35 +0200

    update zad5

--- zad5.cc
+++ zad5.cc
@@ -1,20 +1,16 @@
 #include <iostream>
 
 int main() {
-
-    int l = 0, h = 1000000, a;
+    using std::cin; using std::cout; using std::endl;
     char c;
-
-    std::cout << "Pomysl liczbe od 1 do 1 000 000" << std::endl;
+    int l = 0, h = 1000000, a;
+    cout << "Pomysl liczbe od 1 do 1000000" << endl;
     do {
         a = (l + h) / 2;
-        std::cout << "Czy jest to " << a << "? ";
-        std::cin >> c;
-        if (c == 's') l = a;
-        else if (c == 'b') h = a;
-        else if (c == 'y')
-            std::cout << "Pomyslana liczba to " << a << std::endl;
-    } while (std::cin && c != 'y');
-
+        cout << "Czy jest to " << a << " (y=tak, s=za mala, b=za duza) ? ";
+        cin >> c;
+        if (c == 'y') cout << "Pomyslana liczba to " << a << endl;
+        else if (c == 's') l = a + 1;
+        else if (c == 'b') h = a - 1;
+    } while (cin && c != 'y');
 }
-
