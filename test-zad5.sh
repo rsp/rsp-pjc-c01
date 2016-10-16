54c301af58fae8c8372ce193226a20307350d23f
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-16 12:28:09 +0200

    add runtime tests for zad5

---
+++ test-zad5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+[ -n "$1" ] && m=$1 || m=123456
+
+zi=$(mktemp -u)
+mkfifo -m 600 $zi
+zo=$(mktemp -u)
+mkfifo -m 600 $zo
+
+exec stdbuf -o0 ./guess < $zi | stdbuf -i0 -o0 tr '?' '\n' > $zo &
+p=$!
+
+exec 8< $zo 9> $zi
+
+read a <&8
+echo "$a"
+echo "(thinking $m)"
+g=0
+n=-1
+while read a <&8; do
+n=${a//[^0-9]/}
+echo "$a"
+[[ $n -eq $m && $a =~ Your ]] && continue
+[ $n -eq $m ] && echo "($n - YES)" && echo y >&9
+[ $n -lt $m ] && echo "($n - too small)" && echo s >&9
+[ $n -gt $m ] && echo "($n - too big)" && echo b >&9
+((g++))
+done
+echo "($g questions asked)"
+[ $n -eq $m ] && echo "(TEST PASSED)" || (echo "(TEST FAILED)" && exit 1)
+
