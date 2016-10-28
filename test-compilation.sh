1d9fa5304036fe3f3c95794944d11ea68b63839a
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-28 12:40:16 +0200

    update compilation tests for new filenames

--- test-compilation.sh
+++ test-compilation.sh
@@ -5,37 +5,37 @@ fail () {
 }
 echo GCC VERSION:
 gcc -v
-echo zad1 should build with no defines:
-g++ -o zad1 zad1.cc -Wall -Wfatal-errors || fail
+echo z01 should build with no defines:
+g++ -o z01 z01.cc -Wall -Wfatal-errors || fail
 echo OK
-echo zad2 should not build with no defines:
-g++ -o zad2-test zad2.cc -Wall -Wfatal-errors && fail
+echo z02 should not build with no defines:
+g++ -o z02-test z02.cc -Wall -Wfatal-errors && fail
 echo OK
-echo zad2 should build with GETMIN defined:
-g++ -o zad2-test  zad2.cc -Wall -Wfatal-errors -DGETMIN || fail
+echo z02 should build with GETMIN defined:
+g++ -o z02-test  z02.cc -Wall -Wfatal-errors -DGETMIN || fail
 echo OK
-echo zad2 should build with GETMAX defined:
-g++ -o zad2-test zad2.cc -Wall -Wfatal-errors -DGETMAX || fail
+echo z02 should build with GETMAX defined:
+g++ -o z02-test z02.cc -Wall -Wfatal-errors -DGETMAX || fail
 echo OK
-echo zad2 should build with both GETMIN and GETMAX defined:
-g++ -o zad2-test zad2.cc -Wall -Wfatal-errors -DGETMIN -DGETMAX || fail
+echo z02 should build with both GETMIN and GETMAX defined:
+g++ -o z02-test z02.cc -Wall -Wfatal-errors -DGETMIN -DGETMAX || fail
 echo OK
-echo zad3 should build with no defines:
-g++ -o zad3-test zad3.cc -Wall -Wfatal-errors || fail
+echo z03 should build with no defines:
+g++ -o z03-test z03.cc -Wall -Wfatal-errors || fail
 echo OK
-echo zad4 should not build with no defines:
-g++ -o zad4-test zad4.cc -Wall -Wfatal-errors && fail
+echo z04 should not build with no defines:
+g++ -o z04-test z04.cc -Wall -Wfatal-errors && fail
 echo OK
-echo zad4 should build with ENG defined:
-g++ -o zad4-test zad4.cc -Wall -Wfatal-errors -DENG || fail
+echo z04 should build with ENG defined:
+g++ -o z04-test z04.cc -Wall -Wfatal-errors -DENG || fail
 echo OK
-echo zad4 should build with POL defined:
-g++ -o zad4-test zad4.cc -Wall -Wfatal-errors -DPOL || fail
+echo z04 should build with POL defined:
+g++ -o z04-test z04.cc -Wall -Wfatal-errors -DPOL || fail
 echo OK
-echo zad4 should not build with both ENG and POL defined:
-g++ -o zad4-test zad4.cc -Wall -Wfatal-errors -DENG -DPOL && fail
+echo z04 should not build with both ENG and POL defined:
+g++ -o z04-test z04.cc -Wall -Wfatal-errors -DENG -DPOL && fail
 echo OK
-echo zad5 should build with no defines:
-g++ -o zad5-test zad5.cc -Wall -Wfatal-errors -DENG -DPOL || fail
+echo z05 should build with no defines:
+g++ -o z05-test z05.cc -Wall -Wfatal-errors -DENG -DPOL || fail
 echo OK
-rm zad?-test
+rm z0?-test
